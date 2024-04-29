687c81508576150eec629ed199952badd02fcd10
thatcoderagain <gaurav.officialsites@gmail.com>
2024-04-30 01:42:23 +0530

    sourav work

--- sourav/chapter-3/5B A.c
+++ sourav/chapter-3/5B A.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int pay ,over;
-    printf("enter the time you worked extr  : ");
-    ("")
+    int pay, over;
+    printf("enter the time you worked(at least 40) : ");
+    scanf("%d",&pay);
+    if (pay>40)
+    {
+        over=pay-40;
+        printf("your overtime pay is %d",over*12);
+    }
+    else
+        printf("no overtime pay");
+    return 0;
+
 }
